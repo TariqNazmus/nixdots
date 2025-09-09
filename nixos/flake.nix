743f5ee660eeba17b04013897bbd953038fc868d
TariqNazmus <tariq.nazmus@gmail.com>
2025-09-09 10:54:38 +0000

    fix flake 1

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -11,40 +11,17 @@
       specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
+        ./display-manager.nix
+        ./hyprland.nix
         ./hardware-configuration.nix
         ./nix-settings.nix
         ./nixpkgs.nix
-        ./gc.nix
-        ./screen.nix
-        ./display-manager.nix
-        ./theme.nix
-        ./fonts.nix
+        ./programming-languages.nix
         ./security-services.nix
         ./services.nix
-        # ./printing.nix
-        # ./gnome.nix
-        ./hyprland.nix
-        ./environment-variables.nix
-        ./bluetooth.nix
-        ./networking.nix
-        # ./mac-randomize.nix
-        # ./open-ssh.nix
-        ./mosh.nix
-        ./firewall.nix
-        ./dns.nix
-        ./vpn.nix
+        ./theme.nix
         ./users.nix
-        ./virtualisation.nix
-        ./programming-languages.nix
-        ./lsp.nix
-        ./rust.nix
-        ./radicle.nix
-        ./wasm.nix
-        ./info-fetchers.nix
         ./utils.nix
-        ./terminal-utils.nix
-        ./llm.nix
-        ./work.nix
       ];
     };
   };
