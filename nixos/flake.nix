46e9abd99b93d576122d2b3adfdf22d6d2002dbf
TariqNazmus <tariq.nazmus@gmail.com>
2025-09-09 15:07:19 +0000

    flake take 3

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -10,7 +10,19 @@
     nixosConfigurations.sadat = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
       modules = [
+        ./bluetooth.nix
         ./configuration.nix
+        ./display-manager.nix
+        ./hardware-configuration.nix
+        ./hyprland.nix
+        ./nix-settings.nix
+        ./nixpkgs.nix
+        ./programming-languages.nix
+        ./security-services.nix
+        ./services.nix
+        ./theme.nix
+        ./users.nix
+        ./utils.nix
       ];
     };
   };
